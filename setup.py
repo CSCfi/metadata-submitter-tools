3b22c203ef31806e7475e0d613329240d7456951
Stefan Negru <stefan@blankdots.com>
2021-08-10 14:14:28 +0300

    fix versions and bump tests to 3,8

--- setup.py
+++ setup.py
@@ -8,20 +8,16 @@ with open("requirements.txt") as reqs:
 setup(
     # Core metadata specification here:
     # https://packaging.python.org/specifications/core-metadata/
-    name='metadata-submitter-tools',  # Required
+    name="metadata-submitter-tools",  # Required
     version=__version__,  # Required
     description=__title__,  # Required
     author=__author__,  # Optional
-
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     install_requires=requirements,
-    extras_require={
-        'test': ['coverage', 'pytest', 'pytest-cov',
-                 'coveralls', 'tox']
-    },
-    entry_points='''
+    extras_require={"test": ["coverage==5.5", "pytest==6.2.4", "pytest-cov==2.12.1", "coveralls==3.2.0", "tox=3.24.1"]},
+    entry_points="""
         [console_scripts]
         xml-validate=validator.__main__:cli
-    ''',
+    """,
 )
